9d24163166074e5b2b77f596895113d3cd9bd125
SHISHIR KUSHAWAHA <srktcg@gmail.com>
2022-03-29 12:31:29 +0530

    Add files via upload
    This script can be used to get the all employees under specific manager from Active Directory

---
+++ managerTreeStructure.ps1
@@ -0,0 +1,52 @@
+﻿function addobject($finalreport,$emp,$type)
+{
+    $tempReport=$null
+    $tempReport=[pscustomobject]@{
+    'Name'=$emp.name
+    'Emp ID'=$emp.samaccountname
+    'Email'=$emp.userprincipalname
+    'Manager'=$type
+    }
+    return $tempReport
+}
+
+#variables
+$stack = New-Object System.Collections.Stack
+$finalreport=@()
+Clear-Host
+$main=read-host -Prompt "Enter samaccount name"
+$main_result=get-aduser $main
+$main_result_DN=(get-aduser $main).distinguishedname 
+
+$managedbby=Get-ADUser -Filter { Manager -eq $main_result_dn} | Select-Object Name,samaccountname,distinguishedname,userprincipalname
+if($null -eq $managedbby)
+{
+    $tempReport=addobject $finalreport $main_result $false
+    $finalreport+=$tempReport
+}
+else
+{
+    $tempReport=addobject $finalreport $main_result $true
+    $finalreport+=$tempReport
+    $managedbby | ForEach-Object { $stack.Push($_) }
+}
+while($stack.Count -gt 0 -and ($item = $stack.Pop())) 
+{
+    $employee=$null
+    $DN=$($item.distinguishedname)
+    $employee=Get-ADUser -Filter { Manager -eq $DN } | Select-Object Name,samaccountname,distinguishedname,userprincipalname
+    if($null -ne $employee)
+    {
+        Write-Host "Processing manager $($item.samaccountname)"
+        $tempReport=addobject $finalreport $item $true
+        $finalreport+=$tempReport
+        $employee | ForEach-Object { $stack.Push($_) }
+    }
+    else
+    {
+        Write-Host "Processing employee $($item.samaccountname)"
+        $tempReport=addobject $finalreport $item $false
+        $finalreport+=$tempReport
+    }
+}
+$finalreport
